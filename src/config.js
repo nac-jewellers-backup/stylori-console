1a2abd0175e8659d9a1f9270da8686fc66448efa
manokarantk <manotk.developer@gmail.com>
2019-10-12 02:28:18 +0530

    upload s3 bucket

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-
+export const GRAPHQL_DEV_CLIENT = "http://auth-dev.ap-south-1.elasticbeanstalk.com/graphql"
+export const API_URL = "http://auth-dev.ap-south-1.elasticbeanstalk.com"
 
 // export const GRAPHQL_DEV_CLIENT = "http://localhost:8000/graphql"
 // export const API_URL = "http://localhost:8000"
