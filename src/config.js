066d345d4a3959410023f13119570f3b896ea073
Crayond <crayond@Crayonds-iMac.local>
2020-03-12 20:02:29 +0530

    change url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // export const GRAPHQL_DEV_CLIENT = "http://auth-dev.ap-south-1.elasticbeanstalk.com/graphql"
 // export const API_URL = "http://auth-dev.ap-south-1.elasticbeanstalk.com"
 
-export const API_URL = "https://api-alpha.stylori.net"
-export const GRAPHQL_DEV_CLIENT = "https://api-alpha.stylori.net/graphql"
+export const API_URL = "https://api-beta.stylori.com/"
+export const GRAPHQL_DEV_CLIENT = "https://api-beta.stylori.com/graphql"
 
 // export const GRAPHQL_DEV_CLIENT = "http://192.168.1.105:8000/graphql"
 // export const API_URL = "http://192.168.1.105:8000"
