9f1f903e707de3337e6bca482d757025844487c1
Crayond <crayond@Crayonds-iMac.local>
2020-03-13 09:07:22 +0530

    vendor price setup

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // export const GRAPHQL_DEV_CLIENT = "http://auth-dev.ap-south-1.elasticbeanstalk.com/graphql"
 // export const API_URL = "http://auth-dev.ap-south-1.elasticbeanstalk.com"
-
+export const BASE_URL = "https://api-beta.stylori.com/"
 export const API_URL = "https://api-beta.stylori.com/"
 export const GRAPHQL_DEV_CLIENT = "https://api-beta.stylori.com/graphql"
 
