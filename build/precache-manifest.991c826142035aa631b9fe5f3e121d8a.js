54ca3fa7ad9ca6d14fe88a31174988038b6e6905
Crayond <crayond@Crayonds-iMac.local>
2020-03-13 11:04:04 +0530

    update changes

---
+++ build/precache-manifest.991c826142035aa631b9fe5f3e121d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602ee725b5186125a39461fef26f9888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a3ce7be67944ebc544e",
+    "url": "/static/css/2.c72d5e46.chunk.css"
+  },
+  {
+    "revision": "248dc63d2a118c110f4b",
+    "url": "/static/css/main.2f4d8001.chunk.css"
+  },
+  {
+    "revision": "4a3ce7be67944ebc544e",
+    "url": "/static/js/2.ad979ff3.chunk.js"
+  },
+  {
+    "revision": "248dc63d2a118c110f4b",
+    "url": "/static/js/main.3ea23a42.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // export const API_URL = "http://auth-dev.ap-south-1.elasticbeanstalk.com"
 export const BASE_URL = "https://api-beta.stylori.com/"
 export const API_URL = "https://api-beta.stylori.com/"
-export const GRAPHQL_DEV_CLIENT = "https://api-beta.stylori.com/graphql
+export const GRAPHQL_DEV_CLIENT = "https://api-beta.stylori.com/graphql"
 // export const GRAPHQL_DEV_CLIENT = "http://192.168.1.105:8000/graphql"
 // export const API_URL = "http://192.168.1.105:8000"
