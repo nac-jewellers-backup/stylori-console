1ba5bb88e6b70485bfc2c71af4dca4c99c739120
Mano <manokarantk@gmail.com>
2020-03-23 10:31:50 +0530

    update voucher discount

---
+++ build/precache-manifest.404612bd1e806cdafba40880f730274b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9489f592c2366f36edf121f6661899f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7aadd7fbf22bed469c9",
+    "url": "/static/css/2.c72d5e46.chunk.css"
+  },
+  {
+    "revision": "001c00979cfb02e523be",
+    "url": "/static/css/main.58000353.chunk.css"
+  },
+  {
+    "revision": "a7aadd7fbf22bed469c9",
+    "url": "/static/js/2.ac895113.chunk.js"
+  },
+  {
+    "revision": "001c00979cfb02e523be",
+    "url": "/static/js/main.ad5ec7e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
