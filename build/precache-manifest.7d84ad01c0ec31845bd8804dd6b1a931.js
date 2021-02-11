9a1bcf2828861fccbac98623cf628f1a919d46eb
Samir <samirsaji13@gmail.com>
2021-02-11 17:10:30 +0530

    after build  orderlist and bangles sizes

---
+++ build/precache-manifest.7d84ad01c0ec31845bd8804dd6b1a931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c276218b346d7fda36fb48fca7f38129",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e77bd39a478c136869dd",
+    "url": "./static/css/2.cd95f556.chunk.css"
+  },
+  {
+    "revision": "492e6adcdf54fb9c1520",
+    "url": "./static/css/main.9b31d445.chunk.css"
+  },
+  {
+    "revision": "e77bd39a478c136869dd",
+    "url": "./static/js/2.fb7fc0f9.chunk.js"
+  },
+  {
+    "revision": "492e6adcdf54fb9c1520",
+    "url": "./static/js/main.56d813d7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
