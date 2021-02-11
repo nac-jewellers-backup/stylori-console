68c98745516ce244363b6accfdc4578acb221a4b
Samir <samirsaji13@gmail.com>
2021-02-11 19:09:38 +0530

    orderlist bug

---
+++ build/precache-manifest.df51004810ff31b1af9b54720d912115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cdd3e0e64fbffcc55642fbd0635b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8ba27f8c73fa65a3337",
+    "url": "/static/css/2.cd95f556.chunk.css"
+  },
+  {
+    "revision": "841ef32cc48619e56129",
+    "url": "/static/css/main.9b31d445.chunk.css"
+  },
+  {
+    "revision": "d8ba27f8c73fa65a3337",
+    "url": "/static/js/2.e39d16b0.chunk.js"
+  },
+  {
+    "revision": "841ef32cc48619e56129",
+    "url": "/static/js/main.a265c680.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
