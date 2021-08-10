5889386023d3b8be5b36f60aed137118e2172335
vardhman <vardhman@crayond.co>
2021-08-10 12:23:15 +0530

    added update attributes

---
+++ build/precache-manifest.42eef6115a2dd65a18b5faa1cc2591b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fade8c3f1707a584a9c7da4ae019c768",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e5f4f632e3306de7e20",
+    "url": "/static/css/2.5a4d534d.chunk.css"
+  },
+  {
+    "revision": "04c2d6227791fa1919ed",
+    "url": "/static/css/main.c69f60e7.chunk.css"
+  },
+  {
+    "revision": "4e5f4f632e3306de7e20",
+    "url": "/static/js/2.668ea69d.chunk.js"
+  },
+  {
+    "revision": "04c2d6227791fa1919ed",
+    "url": "/static/js/main.02b5d5db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
