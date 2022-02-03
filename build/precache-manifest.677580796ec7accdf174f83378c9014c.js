58e8b434d9ba91ffb6cf01e21e3d2cc44b79b85c
vardhman <vardhman@crayond.co>
2022-02-03 17:08:22 +0530

    added payment confirmation email

---
+++ build/precache-manifest.677580796ec7accdf174f83378c9014c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873bf1670c06c2024fb17fe3c683e24a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89cedc5907aa479dc120",
+    "url": "/static/css/2.921fbd53.chunk.css"
+  },
+  {
+    "revision": "f5648ecbd20b4a17ab09",
+    "url": "/static/css/main.91300c0e.chunk.css"
+  },
+  {
+    "revision": "89cedc5907aa479dc120",
+    "url": "/static/js/2.5ce7e7ad.chunk.js"
+  },
+  {
+    "revision": "f5648ecbd20b4a17ab09",
+    "url": "/static/js/main.9ae67563.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
