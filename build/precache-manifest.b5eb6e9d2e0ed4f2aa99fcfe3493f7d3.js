a6e9e79cf9e0d15642dd1aaccca39f5c1140fd6f
vardhman <vardhman@crayond.co>
2022-08-04 16:41:29 +0530

    dynamic filter uploader

---
+++ build/precache-manifest.b5eb6e9d2e0ed4f2aa99fcfe3493f7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54f9b6dc523ae11b19349c877897fdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf50d1d35f51626caf95",
+    "url": "/static/css/2.cd7869ca.chunk.css"
+  },
+  {
+    "revision": "5ad18a68d01b1dff5253",
+    "url": "/static/css/main.91300c0e.chunk.css"
+  },
+  {
+    "revision": "bf50d1d35f51626caf95",
+    "url": "/static/js/2.4b222933.chunk.js"
+  },
+  {
+    "revision": "5ad18a68d01b1dff5253",
+    "url": "/static/js/main.a6ac07f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/screens/DynamicFilters/index.js
@@ -0,0 +1,214 @@
+import React from "react";
+import {
+  TableContainer,
+  Tooltip,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Chip,
+  makeStyles,
+  Typography,
+  Button,
+  Grid,
+} from "@material-ui/core";
+import { NetworkContext, AlertContext } from "../../context";
+import exportFromJSON from "export-from-json";
+import { useQuery } from "react-apollo";
+import { dynamicFilterAttributes } from "../../graphql/query";
+import { Skeleton } from "@material-ui/lab";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import axios from "axios";
+import { API_URL } from "../../config";
+import socketIOClient from "socket.io-client";
+import CircularProgressWithLabel from "../../components/CircularProgress";
+
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    padding: theme.spacing(1),    
+  },  
+  chipCell: {
+    "& > *": {
+      margin: theme.spacing(0.1),
+    },
+  },
+  input: {
+    display: "none",
+  },
+}));
+
+export const DynamicFilters = (props) => {
+  const classes = useStyles();
+  const { sendNetworkRequest } = React.useContext(NetworkContext);
+  const snack = React.useContext(AlertContext);
+
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const socket = socketIOClient(API_URL);
+    socket.on("filter_sync", (data) => {
+      console.log(data);
+      if (data.status !== "completed") {
+        setProgress(data?.completed * 100);
+      } else {
+        setProgress(100);
+        snack.setSnack({
+          severity: "info",
+          msg: `Process Completed`,
+        });
+        socket.close();
+      }
+    });
+  }, []);
+
+  const downloadProductFilters = () => {
+    sendNetworkRequest("/fetch_filters")
+      .then((response) => {
+        if (response?.result) {
+          exportFromJSON({
+            data: response.result,
+            fileName: `Product_Filters_${new Date().toLocaleDateString()}`,
+            exportType: "csv",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        snack.setSnack({
+          open: true,
+          severity: "error",
+          msg: "Something went wrong!",
+        });
+      });
+  };
+
+  const handleUpload = (file) => {
+    var bodyFormData = new FormData();
+    bodyFormData.set("file", file);
+    axios
+      .post(API_URL + "/bulk_upload_filters", bodyFormData)
+      .then((res) => {
+        if (res) {
+          setProgress(1);
+          snack.setSnack({
+            open: true,
+            msg: res?.data?.message,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        snack.setSnack({
+          open: true,
+          severity: "error",
+          msg: "Some error occured!",
+        });
+      });
+  };
+
+  let { loading, data, error } = useQuery(dynamicFilterAttributes);
+  return (
+    <>
+      <Grid
+        container
+        spacing={1}        
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        className={classes.grid}
+      >
+        <Grid item>
+          <Tooltip title="Download">
+            <Button
+              variant="outlined"
+              onClick={() => downloadProductFilters()}
+              startIcon={<GetAppIcon />}
+            >
+              Applied Product filters
+            </Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <input
+            accept=".csv"
+            className={classes.input}
+            id="icon-button-file"
+            type="file"
+            onChange={(event) => {
+              const files = event.target.files;
+              if (files) {
+                handleUpload(files[0]);
+              }
+            }}
+          />
+          <label htmlFor="icon-button-file">
+            <Button
+              variant="outlined"
+              startIcon={<CloudUploadIcon />}
+              component="span"
+            >
+              Upload & Apply Product filters
+            </Button>
+          </label>
+        </Grid>
+        <Grid item>
+          {progress > 0 && <CircularProgressWithLabel value={progress} />}
+        </Grid>
+      </Grid>
+
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              {["Name", "Attribute Value"].map((item, index) => (
+                <TableCell
+                  key={index}
+                  style={index == 0 ? { width: 130 } : {}}
+                  align="center"
+                >
+                  {item}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {loading && (
+              <TableRow>
+                <TableCell colSpan={2}>
+                  <Skeleton variant="rect" />
+                </TableCell>
+              </TableRow>
+            )}
+            {!loading &&
+              data?.master?.nodes.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell style={{ width: 130 }} align="center">
+                    <Typography variant="h5">{item?.name}</Typography>
+                  </TableCell>
+                  <TableCell className={classes.chipCell}>
+                    {item?.attributes?.nodes.map((element, i) => (
+                      <Chip
+                        key={i}
+                        size="small"
+                        label={element?.name}
+                        variant="outlined"
+                      />
+                    ))}
+                  </TableCell>
+                </TableRow>
+              ))}
+            {error && (
+              <TableRow>
+                <TableCell colSpan={2} align={"center"}>
+                  <Typography variant="body2">Some Error Occured</Typography>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
