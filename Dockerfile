4f4b2bd976937e0b2587ac69ccb094c998851a72
vardhman <vardhman@crayond.co>
2022-09-14 12:33:38 +0530

    reset docker changes

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
-# Building the app
-FROM node:14-alpine as build 
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . ./
-RUN npm run build
-
 # Deploying the app
 FROM nginx:latest
 COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
-COPY --from=build /app/build /usr/share/nginx/html
+COPY ./build /usr/share/nginx/html
 EXPOSE 80
---
+++ build/precache-manifest.48aacf5c2d829576674657445dcc08cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bc385b7a2e7d23407138faa31666be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76f5c3f48f80817bc0d3",
+    "url": "/static/css/2.cd7869ca.chunk.css"
+  },
+  {
+    "revision": "ca93df907812703d8773",
+    "url": "/static/css/main.91300c0e.chunk.css"
+  },
+  {
+    "revision": "76f5c3f48f80817bc0d3",
+    "url": "/static/js/2.749056aa.chunk.js"
+  },
+  {
+    "revision": "ca93df907812703d8773",
+    "url": "/static/js/main.75281d9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
