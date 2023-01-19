aa208204aa72df0d06a17a8fef0f98a98c42c98a
renalsalex <renals@cryond.co>
2023-01-19 12:14:04 +0530

    CMS completed for all the pages with clone and edit Url features. Commited by renals on 19/1/2023 at 12:13PM

---
+++ build/precache-manifest.ca1855197876a662e765d2154cf189d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36e684c59bf7a132f921c55a851314e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345ebc969bee0fe85dc9",
+    "url": "/static/css/2.a8efd0a0.chunk.css"
+  },
+  {
+    "revision": "f5f91083dcd7e2467d01",
+    "url": "/static/css/main.af7356a7.chunk.css"
+  },
+  {
+    "revision": "345ebc969bee0fe85dc9",
+    "url": "/static/js/2.e6a93ec3.chunk.js"
+  },
+  {
+    "revision": "f5f91083dcd7e2467d01",
+    "url": "/static/js/main.d6f385a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
