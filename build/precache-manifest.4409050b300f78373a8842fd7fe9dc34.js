ee966c31f5343270847dd5f0fc79850e2a263618
renalsalex <renals@cryond.co>
2023-02-16 17:54:06 +0530

    CMS pages FAQ, privacy and T&C added with stylori silver bugs cleared. Commited by renals on 16/2/2023 at 5:45 PM

---
+++ build/precache-manifest.4409050b300f78373a8842fd7fe9dc34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85b09383d8b1e8c38db08425e79661d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9db6634bc0db446c40",
+    "url": "/static/css/2.a8efd0a0.chunk.css"
+  },
+  {
+    "revision": "fa4300b79043a5ec6a8a",
+    "url": "/static/css/main.af7356a7.chunk.css"
+  },
+  {
+    "revision": "8e9db6634bc0db446c40",
+    "url": "/static/js/2.1496134f.chunk.js"
+  },
+  {
+    "revision": "fa4300b79043a5ec6a8a",
+    "url": "/static/js/main.da5fcc39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
