52569953838e68ad038f4ad824870bae4d2bd0c6
Renals Alex <renals@crayond.co>
2023-04-12 23:22:09 +0530

    Blogs page has been added.Commited by renals at 11:21Pm on 12/4/2023

---
+++ build/precache-manifest.e4dd3d6a4e56aaf62c7ec31653adccc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626801e734a4a307288f2b01faea806d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "559ad7908d556d5a7b76",
+    "url": "/static/css/2.49082181.chunk.css"
+  },
+  {
+    "revision": "2a5debf0dafaf188cc16",
+    "url": "/static/css/main.af7356a7.chunk.css"
+  },
+  {
+    "revision": "559ad7908d556d5a7b76",
+    "url": "/static/js/2.58ac7628.chunk.js"
+  },
+  {
+    "revision": "2a5debf0dafaf188cc16",
+    "url": "/static/js/main.a2847aad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
