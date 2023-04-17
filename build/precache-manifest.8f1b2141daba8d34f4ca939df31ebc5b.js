ce99e78f6fbf03982bd1ea26b826dbe0e312f83d
Renals Alex <renals@crayond.co>
2023-04-17 19:06:11 +0530

    Instagram added and the shop get directions issue fixed in the silver stylori. Commited by renals at 7:05pm at 17/4/2023

---
+++ build/precache-manifest.8f1b2141daba8d34f4ca939df31ebc5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e544e6e75df9d13cdea6b191146961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f54ccab2b7ab2c5c84b",
+    "url": "/static/css/2.49082181.chunk.css"
+  },
+  {
+    "revision": "17ad74201947007fca00",
+    "url": "/static/css/main.af7356a7.chunk.css"
+  },
+  {
+    "revision": "8f54ccab2b7ab2c5c84b",
+    "url": "/static/js/2.e07e7afd.chunk.js"
+  },
+  {
+    "revision": "17ad74201947007fca00",
+    "url": "/static/js/main.47ae6f3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
