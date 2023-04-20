78684478104f292a343883b69edaacbaaec2788f
Renals Alex <renals@crayond.co>
2023-04-20 12:47:10 +0530

    make an order flow has been added. Commited by Renals on 20/4/2023 at 12:46PM

---
+++ build/precache-manifest.b282ca07f7d8cb2f5c26fb6e69586066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea80ad2803a932ff3b01bd16e5702b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f54ccab2b7ab2c5c84b",
+    "url": "/static/css/2.49082181.chunk.css"
+  },
+  {
+    "revision": "c0be05aa7ac830ee49ab",
+    "url": "/static/css/main.af7356a7.chunk.css"
+  },
+  {
+    "revision": "8f54ccab2b7ab2c5c84b",
+    "url": "/static/js/2.e07e7afd.chunk.js"
+  },
+  {
+    "revision": "c0be05aa7ac830ee49ab",
+    "url": "/static/js/main.69fcc391.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
